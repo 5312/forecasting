2483b0b0d95f562a2365b0ae4037ddd96a08c217
5312 <1765080897@qq.com>
2021-11-30 19:09:05 +0800

    danger diaocha

--- src/views/tool/example/hiddendanger/hiddendanger-edit.vue
+++ src/views/tool/example/hiddendanger/hiddendanger-edit.vue
@@ -1,124 +1,118 @@
 <!-- 隐患调查编辑弹窗 -->
 <template>
   <el-dialog
-    :title="isUpdate?'修改隐患调查':'添加隐患调查'"
+    :title="isUpdate ? '修改隐患调查' : '添加隐患调查'"
     :visible="visible"
     width="460px"
     :destroy-on-close="true"
     :lock-scroll="false"
-    @update:visible="updateVisible">
-    <el-form
-      ref="form"
-      :model="form"
-      :rules="rules"
-      label-width="82px">
-      
-      
-      
+    @update:visible="updateVisible"
+  >
+    <el-form ref="form" :model="form" :rules="rules" label-width="82px">
       <el-form-item label="单位ID:" prop="dept_id">
         <el-input-number
           :min="0"
           v-model="form.dept_id"
           placeholder="请输入单位ID"
           controls-position="right"
-          class="ele-fluid ele-text-left"/>
+          class="ele-fluid ele-text-left"
+        />
       </el-form-item>
-      
-      
-      
-      
-      
+
       <el-form-item label="类别:" prop="itemcate_id">
         <el-input-number
+          disabled
           :min="0"
           v-model="form.itemcate_id"
           placeholder="请输入类别"
           controls-position="right"
-          class="ele-fluid ele-text-left"/>
+          class="ele-fluid ele-text-left"
+        />
       </el-form-item>
-      
-      
-      
-      
-      
+
       <el-form-item label="栏目:" prop="itemcate_cid">
         <el-input-number
+          disabled
           :min="0"
           v-model="form.itemcate_cid"
           placeholder="请输入栏目"
           controls-position="right"
-          class="ele-fluid ele-text-left"/>
+          class="ele-fluid ele-text-left"
+        />
       </el-form-item>
-      
-      
-      
-      
-      
-      <el-form-item
-        label="隐患标题:"
-        prop="title">
+
+      <el-form-item label="隐患标题:" prop="title">
         <el-input
           :maxlength="20"
           v-model="form.title"
           placeholder="请输入隐患标题"
-          clearable/>
+          clearable
+        />
       </el-form-item>
-      
-      
-      
-      
-      
-      <el-form-item
-        label="评价标准:"
-        prop="score_id">
-        <el-input
+
+      <el-form-item label="评价标准:" prop="score_id">
+        <el-popover
+          placement="bottom"
+          width="450"
+          trigger="click"
+          v-model="show"
+        >
+          <ele-pro-table
+            v-if="visible"
+            ref="table"
+            placement="top"
+            highlight-current-row
+            :current.sync="current"
+            :datasource="url"
+            :columns="columns"
+            :where="where"
+            height="350px"
+            @done="done"
+            :toolbar="false"
+          >
+            <template slot="toolbar"></template>
+          </ele-pro-table>
+          <!--   -->
+          <el-input
+            v-loading="popover"
+            slot="reference"
+            :maxlength="20"
+            readonly=""
+            v-model="setId"
+            placeholder="请选择评价标准"
+            clearable
+          />
+        </el-popover>
+        <!-- <el-input
           :maxlength="20"
           v-model="form.score_id"
           placeholder="请输入评价标准"
-          clearable/>
+          clearable
+        /> -->
       </el-form-item>
-      
-      
-      
-      
-      
-      <el-form-item
-        label="排序:"
-        prop="sort">
+
+      <el-form-item label="排序:" prop="sort">
         <el-input
           :maxlength="20"
           v-model="form.sort"
           placeholder="请输入排序"
-          clearable/>
+          clearable
+        />
       </el-form-item>
-      
-      
-      
     </el-form>
     <div slot="footer">
       <el-button @click="updateVisible(false)">取消</el-button>
-      <el-button
-        type="primary"
-        @click="save"
-        :loading="loading">保存
+      <el-button type="primary" @click="save" :loading="loading"
+        >保存
       </el-button>
     </div>
   </el-dialog>
 </template>
 
 <script>
-
-
-
-
-
-
-
 export default {
-  name: 'HiddendangerEdit',
-  components: {
-
-  },
+  name: "HiddendangerEdit",
+  components: {},
   props: {
     // 弹窗是否打开
     visible: Boolean,
@@ -128,133 +122,128 @@ export default {
   data() {
     return {
       // 表单数据
-      form: Object.assign({}, this.data),
+      form: Object.assign(
+        {
+          score_id: ""
+        },
+        this.data
+      ),
       // 表单验证规则
       rules: {
-	
-		
-		
-		deptId: [
-          {required: true, message: '请输入单位ID', trigger: 'blur'}
+        deptId: [{ required: true, message: "请输入单位ID", trigger: "blur" }],
+
+        itemcateId: [
+          { required: true, message: "请输入类别", trigger: "blur" }
         ],
-		
-		
-	
-		
-		
-		itemcateId: [
-          {required: true, message: '请输入类别', trigger: 'blur'}
+
+        itemcateCid: [
+          { required: true, message: "请输入栏目", trigger: "blur" }
         ],
-		
-		
-	
-		
-		
-		itemcateCid: [
-          {required: true, message: '请输入栏目', trigger: 'blur'}
+
+        title: [{ required: true, message: "请输入隐患标题", trigger: "blur" }],
+
+        scoreId: [
+          { required: true, message: "请输入评价标准", trigger: "blur" }
         ],
-		
-		
-	
-		
-		
-		title: [
-          {required: true, message: '请输入隐患标题', trigger: 'blur'}
-        ],
-		
-		
-	
-		
-		
-		scoreId: [
-          {required: true, message: '请输入评价标准', trigger: 'blur'}
-        ],
-		
-		
-	
-		
-		
-		sort: [
-          {required: true, message: '请输入排序', trigger: 'blur'}
-        ],
-		
-		
-	
+
+        sort: [{ required: true, message: "请输入排序", trigger: "blur" }]
       },
+      show: false,
       // 提交状态
       loading: false,
       // 是否是修改
-      isUpdate: false
+      isUpdate: false,
+      //
+      url: "/score/list",
+      columns: [
+        {
+          prop: "title",
+          label: "评价标准"
+        },
+        {
+          prop: "score",
+          label: "资源赋值(R)",
+          width: 100
+        }
+      ],
+      current: null,
+      popover: false
     };
   },
   watch: {
+    current(n) {
+      this.form.score_id = n.id;
+    },
     data() {
-      if (this.data) {
-        this.form = Object.assign({}, this.data);
+      // console.log(this.data);
+      if (this.data.isUpdate) {
+        this.form = Object.assign({ score_id: "" }, this.data);
         this.isUpdate = true;
       } else {
-        this.form = {};
+        this.form = Object.assign({ score_id: "" }, this.data);
         this.isUpdate = false;
       }
     }
   },
   computed: {
-    // 初始化富文本
-    initEditor() {
-      return {
-        height: 300,
-        branding: false,
-        skin_url: '/tinymce/skins/ui/oxide',
-        content_css: '/tinymce/skins/content/default/content.css',
-        language_url: '/tinymce/langs/zh_CN.js',
-        language: 'zh_CN',
-        plugins: 'code print preview fullscreen paste searchreplace save autosave link autolink image imagetools media table codesample lists advlist hr charmap emoticons anchor directionality pagebreak quickbars nonbreaking visualblocks visualchars wordcount',
-        toolbar: 'fullscreen preview code | undo redo | forecolor backcolor | bold italic underline strikethrough | alignleft aligncenter alignright alignjustify | outdent indent | numlist bullist | formatselect fontselect fontsizeselect | link image media emoticons charmap anchor pagebreak codesample | ltr rtl',
-        toolbar_drawer: 'sliding',
-        images_upload_handler: (blobInfo, success, error) => {
-          let file = blobInfo.blob();
-          // 使用axios上传
-          const formData = new FormData();
-          formData.append('file', file, file.name);
-          this.$http.post('/upload/uploadImage', formData).then(res => {
-            if (res.data.code == 0) {
-              success(res.data.data.fileUrl);
-            } else {
-              error(res.data.msg);
-            }
-          }).catch(e => {
-            console.error(e);
-            error(e.message);
-          });
-        },
-        file_picker_types: 'media',
-        file_picker_callback: () => {
-        }
+    where() {
+      let obj = {
+        itemcate_id: "",
+        itemcate_cid: ""
+      };
+      if (this.data) {
+        obj.itemcate_id = this.data.itemcate_id;
+        obj.itemcate_cid = this.data.itemcate_cid;
       }
+      return obj;
+    },
+    // 赋值ID显示
+    setId() {
+      let val = this.form.score_id * 1;
+      let title = "";
+      if (!this.table_data) return;
+      if (typeof val == "number") {
+        if (!this.table_data) return "";
+        this.table_data.forEach(obj => {
+          if (Number(obj.id) == val) {
+            title = obj.title;
+          }
+        });
+      }
+      return title;
     }
   },
   methods: {
+    done(res) {
+      this.table_data = res.data;
+      this.popover = false;
+    },
     /* 保存编辑 */
     save() {
-      this.$refs['form'].validate((valid) => {
+      this.$refs["form"].validate(valid => {
         if (valid) {
           this.loading = true;
-          this.$http[this.form.id ? 'put' : 'post'](this.isUpdate ? '/hiddendanger/update' : '/hiddendanger/add', this.form).then(res => {
-            this.loading = false;
-            if (res.data.code === 0) {
-              this.$message.success(res.data.msg);
-              if (!this.isUpdate) {
-                this.form = {};
+          this.$http[this.form.id ? "put" : "post"](
+            this.isUpdate ? "/hiddendanger/update" : "/hiddendanger/add",
+            this.form
+          )
+            .then(res => {
+              this.loading = false;
+              if (res.data.code === 0) {
+                this.$message.success(res.data.msg);
+                if (!this.isUpdate) {
+                  this.form = { score_id: "" };
+                }
+                this.updateVisible(false);
+                this.$emit("done");
+              } else {
+                this.$message.error(res.data.msg);
               }
-              this.updateVisible(false);
-              this.$emit('done');
-            } else {
-              this.$message.error(res.data.msg);
-            }
-          }).catch(e => {
-            this.loading = false;
-            this.$message.error(e.message);
-          });
+            })
+            .catch(e => {
+              this.loading = false;
+              this.$message.error(e.message);
+            });
         } else {
           return false;
         }
@@ -262,11 +251,10 @@ export default {
     },
     /* 更新visible */
     updateVisible(value) {
-      this.$emit('update:visible', value);
+      this.$emit("update:visible", value);
     }
   }
-}
+};
 </script>
 
-<style scoped>
-</style>
+<style scoped></style>
