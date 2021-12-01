e6a9f07f0ef45cbeb8e64dd474c27d4772611a11
5312 <1765080897@qq.com>
2021-12-01 18:09:02 +0800

    new page /risk/library

---
+++ src/views/tool/example/risklibrary/risklibrary-edit.vue
@@ -0,0 +1,339 @@
+<!-- 安全威胁预选库编辑弹窗 -->
+<template>
+  <el-dialog
+    :title="isUpdate?'修改安全威胁预选库':'添加安全威胁预选库'"
+    :visible="visible"
+    width="460px"
+    :destroy-on-close="true"
+    :lock-scroll="false"
+    @update:visible="updateVisible">
+    <el-form
+      ref="form"
+      :model="form"
+      :rules="rules"
+      label-width="82px">
+      
+      
+      
+      <el-form-item label="评测任务ID:" prop="forecast_id">
+        <el-input-number
+          :min="0"
+          v-model="form.forecast_id"
+          placeholder="请输入评测任务ID"
+          controls-position="right"
+          class="ele-fluid ele-text-left"/>
+      </el-form-item>
+      
+      
+      
+      
+      
+      <el-form-item label="单位ID:" prop="dept_id">
+        <el-input-number
+          :min="0"
+          v-model="form.dept_id"
+          placeholder="请输入单位ID"
+          controls-position="right"
+          class="ele-fluid ele-text-left"/>
+      </el-form-item>
+      
+      
+      
+      
+      
+      <el-form-item
+        label="类别:"
+        prop="yinhuan_ids">
+        <el-input
+          :maxlength="20"
+          v-model="form.yinhuan_ids"
+          placeholder="请输入类别"
+          clearable/>
+      </el-form-item>
+      
+      
+      
+      
+      
+      <el-form-item
+        label="栏目:"
+        prop="ziyuan_ids">
+        <el-input
+          :maxlength="20"
+          v-model="form.ziyuan_ids"
+          placeholder="请输入栏目"
+          clearable/>
+      </el-form-item>
+      
+      
+      
+      
+      
+      <el-form-item label="安全风险库ID:" prop="riskdata_id">
+        <el-input-number
+          :min="0"
+          v-model="form.riskdata_id"
+          placeholder="请输入安全风险库ID"
+          controls-position="right"
+          class="ele-fluid ele-text-left"/>
+      </el-form-item>
+      
+      
+      
+      
+      
+      <el-form-item label="威胁资源ID:" prop="risk_id">
+        <el-input-number
+          :min="0"
+          v-model="form.risk_id"
+          placeholder="请输入威胁资源ID"
+          controls-position="right"
+          class="ele-fluid ele-text-left"/>
+      </el-form-item>
+      
+      
+      
+      
+      
+      <el-form-item
+        label="威胁行为:"
+        prop="title">
+        <el-input
+          :maxlength="20"
+          v-model="form.title"
+          placeholder="请输入威胁行为"
+          clearable/>
+      </el-form-item>
+      
+      
+      
+      
+      
+      <el-form-item label="评价标准:" prop="score_id">
+        <el-input-number
+          :min="0"
+          v-model="form.score_id"
+          placeholder="请输入评价标准"
+          controls-position="right"
+          class="ele-fluid ele-text-left"/>
+      </el-form-item>
+      
+      
+      
+      
+      
+      <el-form-item
+        label="排序:"
+        prop="sort">
+        <el-input
+          :maxlength="20"
+          v-model="form.sort"
+          placeholder="请输入排序"
+          clearable/>
+      </el-form-item>
+      
+      
+      
+    </el-form>
+    <div slot="footer">
+      <el-button @click="updateVisible(false)">取消</el-button>
+      <el-button
+        type="primary"
+        @click="save"
+        :loading="loading">保存
+      </el-button>
+    </div>
+  </el-dialog>
+</template>
+
+<script>
+
+
+
+
+
+
+
+
+
+
+export default {
+  name: 'RisklibraryEdit',
+  components: {
+
+  },
+  props: {
+    // 弹窗是否打开
+    visible: Boolean,
+    // 修改回显的数据
+    data: Object
+  },
+  data() {
+    return {
+      // 表单数据
+      form: Object.assign({}, this.data),
+      // 表单验证规则
+      rules: {
+	
+		
+		
+		forecastId: [
+          {required: true, message: '请输入评测任务ID', trigger: 'blur'}
+        ],
+		
+		
+	
+		
+		
+		deptId: [
+          {required: true, message: '请输入单位ID', trigger: 'blur'}
+        ],
+		
+		
+	
+		
+		
+		yinhuanIds: [
+          {required: true, message: '请输入类别', trigger: 'blur'}
+        ],
+		
+		
+	
+		
+		
+		ziyuanIds: [
+          {required: true, message: '请输入栏目', trigger: 'blur'}
+        ],
+		
+		
+	
+		
+		
+		riskdataId: [
+          {required: true, message: '请输入安全风险库ID', trigger: 'blur'}
+        ],
+		
+		
+	
+		
+		
+		riskId: [
+          {required: true, message: '请输入威胁资源ID', trigger: 'blur'}
+        ],
+		
+		
+	
+		
+		
+		title: [
+          {required: true, message: '请输入威胁行为', trigger: 'blur'}
+        ],
+		
+		
+	
+		
+		
+		scoreId: [
+          {required: true, message: '请输入评价标准', trigger: 'blur'}
+        ],
+		
+		
+	
+		
+		
+		sort: [
+          {required: true, message: '请输入排序', trigger: 'blur'}
+        ],
+		
+		
+	
+      },
+      // 提交状态
+      loading: false,
+      // 是否是修改
+      isUpdate: false
+    };
+  },
+  watch: {
+    data() {
+      if (this.data) {
+        this.form = Object.assign({}, this.data);
+        this.isUpdate = true;
+      } else {
+        this.form = {};
+        this.isUpdate = false;
+      }
+    }
+  },
+  computed: {
+    // 初始化富文本
+    initEditor() {
+      return {
+        height: 300,
+        branding: false,
+        skin_url: '/tinymce/skins/ui/oxide',
+        content_css: '/tinymce/skins/content/default/content.css',
+        language_url: '/tinymce/langs/zh_CN.js',
+        language: 'zh_CN',
+        plugins: 'code print preview fullscreen paste searchreplace save autosave link autolink image imagetools media table codesample lists advlist hr charmap emoticons anchor directionality pagebreak quickbars nonbreaking visualblocks visualchars wordcount',
+        toolbar: 'fullscreen preview code | undo redo | forecolor backcolor | bold italic underline strikethrough | alignleft aligncenter alignright alignjustify | outdent indent | numlist bullist | formatselect fontselect fontsizeselect | link image media emoticons charmap anchor pagebreak codesample | ltr rtl',
+        toolbar_drawer: 'sliding',
+        images_upload_handler: (blobInfo, success, error) => {
+          let file = blobInfo.blob();
+          // 使用axios上传
+          const formData = new FormData();
+          formData.append('file', file, file.name);
+          this.$http.post('/upload/uploadImage', formData).then(res => {
+            if (res.data.code == 0) {
+              success(res.data.data.fileUrl);
+            } else {
+              error(res.data.msg);
+            }
+          }).catch(e => {
+            console.error(e);
+            error(e.message);
+          });
+        },
+        file_picker_types: 'media',
+        file_picker_callback: () => {
+        }
+      }
+    }
+  },
+  methods: {
+    /* 保存编辑 */
+    save() {
+      this.$refs['form'].validate((valid) => {
+        if (valid) {
+          this.loading = true;
+          this.$http[this.form.id ? 'put' : 'post'](this.isUpdate ? '/risklibrary/update' : '/risklibrary/add', this.form).then(res => {
+            this.loading = false;
+            if (res.data.code === 0) {
+              this.$message.success(res.data.msg);
+              if (!this.isUpdate) {
+                this.form = {};
+              }
+              this.updateVisible(false);
+              this.$emit('done');
+            } else {
+              this.$message.error(res.data.msg);
+            }
+          }).catch(e => {
+            this.loading = false;
+            this.$message.error(e.message);
+          });
+        } else {
+          return false;
+        }
+      });
+    },
+    /* 更新visible */
+    updateVisible(value) {
+      this.$emit('update:visible', value);
+    }
+  }
+}
+</script>
+
+<style scoped>
+</style>
