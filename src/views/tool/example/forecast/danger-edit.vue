724ac729c20e12b6fbabfd288e34c018731ff890
5312 <1765080897@qq.com>
2022-05-05 18:32:32 +0800

    添加隐患因素页面 按钮文字改为 添加隐患

--- src/views/tool/example/forecast/danger-edit.vue
+++ src/views/tool/example/forecast/danger-edit.vue
@@ -61,7 +61,7 @@
                 style="float: right; padding: 3px 0; margin-left: 10px"
                 type="primary"
                 @click.stop="addSafeStatus(y, x.id)"
-                >添加</el-button
+                >添加隐患</el-button
               >
               <el-button
                 icon="el-icon-plus"
@@ -129,8 +129,8 @@
 </template>
 
 <script>
-import Danger from "./danger.vue";
-import Prim from "./danger-prim.vue";
+import Danger from "./danger.vue"
+import Prim from "./danger-prim.vue"
 export default {
   name: "ForecastEdit",
   components: { Danger, Prim },
@@ -140,7 +140,7 @@ export default {
     // 修改回显的数据
     data: Object
   },
-  data() {
+  data () {
     return {
       defaultColumns: [
         {
@@ -227,76 +227,76 @@ export default {
       primIndex: null,
       primID: null,
       pData: {}
-    };
+    }
   },
-  mounted() {},
+  mounted () { },
   watch: {
-    data() {
+    data () {
       if (this.data) {
-        this.form = Object.assign({}, this.data);
-        this.isUpdate = true;
+        this.form = Object.assign({}, this.data)
+        this.isUpdate = true
       } else {
-        this.form = {};
-        this.isUpdate = false;
+        this.form = {}
+        this.isUpdate = false
       }
     },
-    visible(val) {
+    visible (val) {
       if (val) {
-        this.index();
+        this.index()
       } else {
-        this.typeData = [];
+        this.typeData = []
       }
     }
   },
   methods: {
     // 数量加减
-    add(row, y) {
+    add (row, y) {
       this.$http
         .put("/hiddendangerlibrary/upsums", {
           id: row.id,
           sums: row.sums
         })
         .then(() => {
-          this.reload(y);
-        });
+          this.reload(y)
+        })
     },
     // 模板
-    temp(arr) {
-      let array = arr;
-      if (!arr) return {};
-      const col = this.defaultColumns;
-      let col_arr = [];
+    temp (arr) {
+      let array = arr
+      if (!arr) return {}
+      const col = this.defaultColumns
+      let col_arr = []
       for (let index = 0; index < col.length; index++) {
-        const element = col[index];
-        col_arr.push(element);
+        const element = col[index]
+        col_arr.push(element)
       }
       for (let i = 0; i < array.length; i++) {
-        const element = array[i];
-        element.prop = element.code;
-        element.label = element.title;
-        element.align = "center";
-        element.width = 180;
-        col_arr.push(element);
+        const element = array[i]
+        element.prop = element.code
+        element.label = element.title
+        element.align = "center"
+        element.width = 180
+        col_arr.push(element)
       }
-      return col_arr;
+      return col_arr
     },
-    parseData_left(res) {
+    parseData_left (res) {
       if (!res.data) {
-        console.log();
-        return { code: 0, data: [] };
+        console.log()
+        return { code: 0, data: [] }
       }
-      let parse = res.data;
-      let num = 0;
+      let parse = res.data
+      let num = 0
       for (let i = 0; i < parse.length; i++) {
-        const element = parse[i];
-        num += element.Scoresum * 1;
+        const element = parse[i]
+        num += element.Scoresum * 1
       }
-      let list_id = parse[0].itemcate_id;
+      let list_id = parse[0].itemcate_id
       for (let i = 0; i < this.typeData.length; i++) {
-        const element = this.typeData[i];
+        const element = this.typeData[i]
         if (element.id == list_id) {
-          let nums = Math.round(num * 100) / 100;
-          element.score = nums;
+          let nums = Math.round(num * 100) / 100
+          element.score = nums
         }
       }
       return {
@@ -305,47 +305,47 @@ export default {
         count: 0,
         data: parse,
         msg: "操作成功"
-      };
+      }
     },
-    reload(i) {
-      this.typeData = [];
-      this.index();
-      let table = `table${i}`;
-      this.$refs[table][0].reload({ page: 1 });
+    reload (i) {
+      this.typeData = []
+      this.index()
+      let table = `table${i}`
+      this.$refs[table][0].reload({ page: 1 })
     },
-    addSafeStatus(x, y) {
-      this.tableIndex = x;
+    addSafeStatus (x, y) {
+      this.tableIndex = x
       // 添加隐患因素
       if (this.tableIndex !== null) {
-        let arr = this.typeData[x].temptlate;
-        let array = this.defaultColumns;
+        let arr = this.typeData[x].temptlate
+        let array = this.defaultColumns
         // 深拷贝
-        array = JSON.stringify(array);
-        array = JSON.parse(array);
+        array = JSON.stringify(array)
+        array = JSON.parse(array)
         for (let i = 0; i < arr.length; i++) {
-          const element = arr[i];
-          array.push(element);
+          const element = arr[i]
+          array.push(element)
         }
-        this.colnums_all = array;
-        this.twoshow = true;
+        this.colnums_all = array
+        this.twoshow = true
       }
-      this.params_id = y;
+      this.params_id = y
     },
     // 打开评分
-    addPrim(i, j) {
-      this.primIndex = i;
-      this.primID = j;
-      this.Portion();
+    addPrim (i, j) {
+      this.primIndex = i
+      this.primID = j
+      this.Portion()
     },
-    Portion() {
+    Portion () {
       const loading = this.$loading({
         lock: true,
         text: "Loading",
         spinner: "el-icon-loading",
         background: "rgba(0, 0, 0, 0.7)"
-      });
+      })
       //  改变data 要不多次点击可鞥不会改变
-      this.pData = { isUpdate: false };
+      this.pData = { isUpdate: false }
       this.$http
         .get("/hiddendangerscorelibrary/list", {
           params: {
@@ -355,94 +355,94 @@ export default {
           }
         })
         .then(res => {
-          loading.close();
-          let obj = this.form;
-          obj.tablerowid = this.data.id;
+          loading.close()
+          let obj = this.form
+          obj.tablerowid = this.data.id
 
-          this.protion = res.data.data;
+          this.protion = res.data.data
           if (res.data.data) {
-            obj.isUpdate = true;
-            obj.id = this.protion[0].id;
+            obj.isUpdate = true
+            obj.id = this.protion[0].id
           } else {
-            obj.isUpdate = false;
+            obj.isUpdate = false
           }
-          this.pData = obj;
-          console.log("--:", obj.isUpdate);
-          this.showPrim = true;
-        });
+          this.pData = obj
+          console.log("--:", obj.isUpdate)
+          this.showPrim = true
+        })
     },
-    manualAdd(x) {
+    manualAdd (x) {
       // 展开项 下表标
-      this.tableIndex = x;
-      this.man = true;
+      this.tableIndex = x
+      this.man = true
     },
-    saveTableData(array) {
-      console.log(array);
+    saveTableData (array) {
+      console.log(array)
       // 添加时，选中的表格数据
-      let i = this.tableIndex;
-      this.reload(i);
-      this.twoshow = false;
+      let i = this.tableIndex
+      this.reload(i)
+      this.twoshow = false
     },
 
-    async remove(row, index) {
-      this.tableIndex = index;
+    async remove (row, index) {
+      this.tableIndex = index
       // 删除
       const res = await this.$http.delete(
         "/hiddendangerlibrary/delete/" + row.id
-      );
+      )
       if (res.data.code == 0) {
-        this.reload(index);
-        const element = this.typeData[index];
-        element.score = 0;
+        this.reload(index)
+        const element = this.typeData[index]
+        element.score = 0
       }
     },
     // 编辑的数据
-    async index() {
+    async index () {
       const loading = this.$loading({
         lock: true,
         text: "Loading",
         spinner: "el-icon-loading",
         background: "rgba(0, 0, 0, 0.7)"
-      });
+      })
       const res = await this.$http.get("/itemcate/list", {
         params: {
           item_id: 2
         }
-      });
+      })
       if (res.data.code == 0) {
-        let table_item = res.data.data || [];
-        this.formate(table_item);
+        let table_item = res.data.data || []
+        this.formate(table_item)
       }
-      loading.close();
+      loading.close()
     },
     // 数据处理
-    async formate(data) {
-      let array = data;
+    async formate (data) {
+      let array = data
       // 生成手风琴 栏目项
       for (let index = 0; index < array.length; index++) {
-        const element = array[index];
+        const element = array[index]
         // pid == 0 顶级节点
         if (element.pid == 0) {
           element.where = {
             forecast_id: this.form.id,
             itemcateid: element.id,
             itemcatecid: null
-          };
+          }
           // 每个表格的 模板不一样
           const d = await this.$http.get("/configdata/list", {
             params: {
               configId: element.id,
               forecast_id: this.form.id
             }
-          });
-          let temp = d.data.data;
-          element.temptlate = temp || [];
+          })
+          let temp = d.data.data
+          element.temptlate = temp || []
           // 表格 数据 接口
-          element.url = "/hiddendangerlibrary/list";
-          element.show = true;
+          element.url = "/hiddendangerlibrary/list"
+          element.show = true
           // 总分
-          element.score = 0;
-          element.children = [];
+          element.score = 0
+          element.children = []
           /*********/
 
           const children_score = await this.$http.get(
@@ -453,94 +453,94 @@ export default {
                 itemcate_id: element.id // 栏目id
               }
             }
-          );
-          let num = children_score.data.data || [];
-          element.children_score = num;
+          )
+          let num = children_score.data.data || []
+          element.children_score = num
           // 1.id == c.pid 当前节点的子节点
           for (let i = 0; i < array.length; i++) {
-            const ele = array[i];
+            const ele = array[i]
             if (element.id == ele.pid) {
-              element.children.push(ele);
-              this.primData.push(ele);
+              element.children.push(ele)
+              this.primData.push(ele)
             }
           }
           /***********/
-          this.typeData.push(element);
+          this.typeData.push(element)
         }
       }
     },
-    com_score(data, i) {
+    com_score (data, i) {
       // 计算当前项 分数;
       for (let index = 0; index < data.length; index++) {
-        const element = data[index];
+        const element = data[index]
         if (element.itemcate_cid == i.id) {
-          return element.Score;
+          return element.Score
         }
       }
-      return 0;
+      return 0
     },
-    all_score(array) {
-      let num = 0;
+    all_score (array) {
+      let num = 0
       for (let i = 0; i < array.length; i++) {
-        const element = array[i];
-        num += Number(element.Score);
+        const element = array[i]
+        num += Number(element.Score)
       }
-      let sum_math = 1 - Math.round(num * 100) / 100;
-      let r = 0;
+      let sum_math = 1 - Math.round(num * 100) / 100
+      let r = 0
       if (sum_math >= 0.9) {
-        r = 1;
+        r = 1
       }
       if (sum_math < 0.9 && sum_math >= 0.8) {
-        r = 2;
+        r = 2
       }
       if (sum_math < 0.8 && sum_math >= 0.7) {
-        r = 3;
+        r = 3
       }
       if (sum_math < 0.7 && sum_math >= 0.6) {
-        r = 4;
+        r = 4
       }
       if (sum_math < 0.6 && sum_math >= 0.5) {
-        r = 5;
+        r = 5
       }
       if (sum_math < 0.5) {
-        r = 5;
+        r = 5
       }
-      return r;
+      return r
     },
     /* 保存编辑 */
-    save() {
+    save () {
       this.$refs["form"].validate(valid => {
         if (valid) {
-          this.loading = true;
+          this.loading = true
           this.$http[this.form.id ? "put" : "post"](
             this.isUpdate ? "/hiddendanger/update" : "/hiddendanger/add",
             this.form
           )
             .then(res => {
-              this.loading = false;
+              this.loading = false
               if (res.data.code === 0) {
-                this.$message.success(res.data.msg);
+                this.$message.success(res.data.msg)
                 if (!this.isUpdate) {
-                  this.form = {};
+                  this.form = {}
                 }
-                this.updateVisible(false);
-                this.$emit("done");
+                this.updateVisible(false)
+                this.$emit("done")
               } else {
-                this.$message.error(res.data.msg);
+                this.$message.error(res.data.msg)
               }
             })
             .catch(e => {
-              this.loading = false;
-              this.$message.error(e.message);
-            });
+              this.loading = false
+              this.$message.error(e.message)
+            })
         } else {
-          return false;
+          return false
         }
-      });
+      })
     },
     /* 更新visible */
-    updateVisible(value) {
-      this.$emit("update:visible", value);
+    updateVisible (value) {
+      this.$emit("update:visible", value)
     }
   }
 };
